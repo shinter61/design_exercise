c83c13819ab6a3c7618a1b20ff3f2966c59f177a
shinter61 <shinter61@gmail.com>
2019-06-09 10:28:27 +0900

    背景画像追加

--- src/Paspol.jsx
+++ src/Paspol.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import style from './paspol.module.scss'
 
 export default class Paspol extends React.Component {
   render() {
     return(
-      <div>heelo world</div>
+      <div>
+        <div className="main_imgBox">
+          <div className="main_img" id="image1"></div>
+          <div className="main_img" id="image2"></div>
+          <div className="main_img" id="image3"></div>
+          <div className="main_img" id="image4"></div>
+          <div className="main_img" id="image5"></div>
+        </div>
+        <div className="content">hello, world</div>
+      </div>
     )
   }
 }
