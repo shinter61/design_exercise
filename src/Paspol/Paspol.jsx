3114373119b7c325ee7c8eec93808469749ba425
shinter61 <shinter61@gmail.com>
2019-06-22 17:09:16 +0900

    フェードイン、フェードアウト実装

--- src/Paspol/Paspol.jsx
+++ src/Paspol/Paspol.jsx
@@ -6,6 +6,8 @@ import image3 from '../../public/image/paspol3.jpg';
 import image4 from '../../public/image/paspol4.jpg';
 import image5 from '../../public/image/paspol5.jpg';
 
+let m = 1
+let n = 0
 export default class Paspol extends React.Component {
   constructor() {
     super();
@@ -14,15 +16,38 @@ export default class Paspol extends React.Component {
     };
   }
   componentDidMount() {
-    this.interval = setInterval(() => {
-      this.slideShow()}, 2000
-    )
+    this.interval = this.slideShow()
   }
 
   componentWillUnmount() {
     clearInterval(this.interval)
   }
 
+  fadeout = () => {
+    if (m < 0) {
+      clearTimeout()
+      console.log('フェードアウト終了')
+      m = 1;
+      return
+    }
+    m -= 0.01;
+    document.getElementById('slideshowPrev').style.opacity = m;
+    console.log(document.getElementById('slideshowPrev').style.opacity)
+    setTimeout(() => {this.fadeout()}, 20);
+  }
+
+  fadein = () => {
+    if (n > 1) {
+      clearTimeout()
+      console.log('フェードイン終了')
+      n = 0;
+      return
+    }
+    n += 0.01;
+    document.getElementById('slideshowNext').style.opacity = n;
+    console.log(document.getElementById('slideshowNext').style.opacity)
+    setTimeout(() => {this.fadein()}, 20);
+  }
   slideShow = () => {
     const imageArr = [image1, image2, image3, image4, image5]
     let num = this.state.imageId
@@ -32,28 +57,25 @@ export default class Paspol extends React.Component {
       this.setState({ imageId: 0 });
     }
     document.getElementById('slideshowPrev').src = imageArr[num]
-    document.getElementById('slideshowPrev').style.opacity = 1;
-    this.fadeInterval = setInterval(() => {this.fadeOut()}, 100)
-    if (num !== 4){
+    if (num !== 4) {
       num++
     } else {
       num = 0
     }
     document.getElementById('slideshowNext').src = imageArr[num]
+    this.fadeout()
+    this.fadein()
+    setTimeout(() => {this.slideShow()}, 5000);
   }
 
-  fadeOut = () => {
-    if (document.getElementById('slideshowPrev').style.opacity === 0) {return}
-    console.log(document.getElementById('slideshowPrev').style.opacity)
-    document.getElementById('slideshowPrev').style.opacity = document.getElementById('slideshowPrev').style.opacity - 0.1
-  }
-  render(){
+
+  render() {
     return (
       <>
         <div className='abovePage'>
           <div className='imageSlideShow'>
-            <img id='slideshowPrev' src={image1} />
-            <img id='slideshowNext' src={image2} />
+            <img id='slideshowPrev' src={image1} style={{opacity: 1}}/>
+            <img id='slideshowNext' src={image2} style={{opacity: 0}}/>
           </div>
         </div>
         <p>hello from paspol</p>
